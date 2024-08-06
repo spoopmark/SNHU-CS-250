ed31458c0856fac4ae14ce840195a0b3a8957050
unknown <rosestiffey@gmail.com>
2024-08-06 10:41:26 -0400

    push

---
+++ Module 5/Slide Show/src/SlideShow.java
@@ -0,0 +1,168 @@
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Color;
+
+public class SlideShow extends JFrame {
+
+	//Declare Variables
+	private JPanel slidePane;
+	private JPanel textPane;
+	private JPanel buttonPane;
+	private CardLayout card;
+	private CardLayout cardText;
+	private JButton btnPrev;
+	private JButton btnNext;
+	private JLabel lblSlide;
+	private JLabel lblTextArea;
+
+	/**
+	 * Create the application.
+	 */
+	public SlideShow() throws HeadlessException {
+		initComponent();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initComponent() {
+		//Initialize variables to empty objects
+		card = new CardLayout();
+		cardText = new CardLayout();
+		slidePane = new JPanel();
+		textPane = new JPanel();
+		textPane.setBackground(Color.BLUE);
+		textPane.setBounds(5, 470, 790, 50);
+		textPane.setVisible(true);
+		buttonPane = new JPanel();
+		btnPrev = new JButton();
+		btnNext = new JButton();
+		lblSlide = new JLabel();
+		lblTextArea = new JLabel();
+
+		//Setup frame attributes
+		setSize(800, 600);
+		setLocationRelativeTo(null);
+		setTitle("Top 5 Destinations SlideShow");
+		getContentPane().setLayout(new BorderLayout(10, 50));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		//Setting the layouts for the panels
+		slidePane.setLayout(card);
+		textPane.setLayout(cardText);
+		
+		//logic to add each of the slides and text
+		for (int i = 1; i <= 5; i++) {
+			lblSlide = new JLabel();
+			lblTextArea = new JLabel();
+			lblSlide.setText(getResizeIcon(i));
+			lblTextArea.setText(getTextDescription(i));
+			slidePane.add(lblSlide, "card" + i);
+			textPane.add(lblTextArea, "cardText" + i);
+		}
+
+		getContentPane().add(slidePane, BorderLayout.CENTER);
+		getContentPane().add(textPane, BorderLayout.SOUTH);
+
+		buttonPane.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10));
+
+		btnPrev.setText("Previous");
+		btnPrev.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				goPrevious();
+			}
+		});
+		buttonPane.add(btnPrev);
+
+		btnNext.setText("Next");
+		btnNext.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				goNext();
+			}
+		});
+		buttonPane.add(btnNext);
+
+		getContentPane().add(buttonPane, BorderLayout.SOUTH);
+	}
+
+	/**
+	 * Previous Button Functionality
+	 */
+	private void goPrevious() {
+		card.previous(slidePane);
+		cardText.previous(textPane);
+	}
+	
+	/**
+	 * Next Button Functionality
+	 */
+	private void goNext() {
+		card.next(slidePane);
+		cardText.next(textPane);
+	}
+
+	/**
+	 * Method to get the images
+	 */
+	private String getResizeIcon(int i) {
+		String image = ""; 
+		if (i==1){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/SHA Wellness Clinic Spain.jpg") + "'</body></html>";
+		} else if (i==2){ // so apparently webp is not an accepted format. this is why you read the docs
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/Kamalaya Thailand.jpg") + "'</body></html>";
+		} else if (i==3){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/Lumeria Maui.jpg") + "'</body></html>";
+		} else if (i==4){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/The Farm At San Benitos.jpg") + "'</body></html>";
+		} else if (i==5){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/COMO Shambhala Estate.jpg") + "'</body></html>";
+		}
+		return image;
+	}
+	
+	/**
+	 * Method to get the text values
+	 */
+	private String getTextDescription(int i) {
+		String text = ""; 
+		if (i==1){
+			text = "<html><body><font size='5'>#1 SHA Wellness Clinic, Spain.</font> <br>Luxurious holistic retreat amidst serene Spanish landscapes.</body></html>";
+		} else if (i==2){ // added "headers" (font size) for the remaining entries
+			text = "<html><body><font size='5'>#2 Kamalaya Wellness Sanctuary & Holistic Spa, Thailand</font> <br>Tropical paradise for spiritual growth and renewal.</html>";
+		} else if (i==3){
+			text = "<html><body><font size='5'>#3 Lumeria Maui Retreat Center</font> <br>Serenely nestled retreat amidst lush Hawaiian nature.</html>";
+		} else if (i==4){
+			text = "<html><body><font size='5'>#4 The Farm At San Benitos, Philippines</font> <br>Holistic wellness retreat for body and soul.</html>";
+		} else if (i==5){
+			text = "<html><body><font size='5'>#5 COMO Shambhala Estate, Indonesia</font> <br>Luxurious wellness retreat for mind and spirit.</html>";
+		}
+		return text;
+	}
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				SlideShow ss = new SlideShow();
+				ss.setVisible(true);
+			}
+		});
+	}
+}
